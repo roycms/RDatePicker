39ded471466e1ec802371c8aa02bb747d93374c9
roycms <roycms@qq.com>
2017-02-24 16:12:46 +0800

    增加注释

--- RDatePicker/ViewController.m
+++ RDatePicker/ViewController.m
@@ -20,13 +20,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
-
+//点击键盘外区域关闭弹出view
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
     if (![self.textField isExclusiveTouch]) {
         [self.textField resignFirstResponder];
     }
 }
-
+ //懒加载 初始化一个UITextField 把UITextField的inputView设置成 RDatePicker
 - (UITextField *)textField {
     if (_textField == nil) {
         RDatePicker *pickview = [[RDatePicker alloc]init];
